59d56dd5ceeb719c01ac6e01b66d69bc83bd6929
nysalvi <nysalvi20@gmail.com>
2023-04-25 23:35:53 -0300

    Added :     - PATH(used to save individual model data);     - Epoch(num of epochs to train);     - BoardX(base description for graphs in TensorBoardX's SummaryWriter object

--- CancroEuropeu/Neural Network/src/utils/model_info.py
+++ CancroEuropeu/Neural Network/src/utils/model_info.py
@@ -1,12 +1,15 @@
 from torch.utils.tensorboard import SummaryWriter
-
 class Info():
     Writer = SummaryWriter()
+    Epoch = 0
     Device = ''
-    Model = ''
+    Name = ''
     Optim = ''
     WeightDecay = 0
     LR_Decay = 0
     LR = 0
     Momentum = 0
     SaveType = ''
+
+    BoardX = f"{Name}/SavedWith_{SaveType}/{Optim}/LR_{LR}/Momentum_{Momentum}"
+    PATH = f'D:/output/{Name}/SaveType_{SaveType}/{Optim}/lr_{LR}/momentum_{Momentum}'
