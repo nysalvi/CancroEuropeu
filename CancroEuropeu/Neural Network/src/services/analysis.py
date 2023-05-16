bba6258c2bef58da11a6b7a68563398160757e40
nysalvi <nysalvi20@gmail.com>
2023-05-16 17:52:31 -0300

    Information included in saved CSV files is now present in folder directory. Names have been simplified

--- CancroEuropeu/Neural Network/src/services/analysis.py
+++ CancroEuropeu/Neural Network/src/services/analysis.py
@@ -8,13 +8,13 @@ import os
 class Analysis:
     @staticmethod
     def run() -> any:
-        df = pd.read_csv(f'{Info.PATH}/{Info.Name}_{Info.Optim}.csv', sep=";", decimal=",")
+        df = pd.read_csv(f'{Info.PATH}/result.csv', sep=";", decimal=",")
         y = df.true_values.values
         scores = -df.outputs_1.values
         precision, recall, thresholds = metrics.precision_recall_curve(y, scores, pos_label=0)
         display = metrics.PrecisionRecallDisplay(precision=precision, recall=recall)
         display.plot()                
-        display.figure_.savefig(f"{Info.PATH}/{Info.Name}_{Info.Optim}_prec_recall.png")
+        display.figure_.savefig(f"{Info.PATH}/prec_recall.png")
 
         best_thresholds = [(scores[i] + scores[i + 1]) / 2 for i, yi in enumerate(y[:-1]) if y[i] != y[i + 1]]
         results = []
@@ -23,7 +23,8 @@ class Analysis:
             report = precision_recall_fscore_support(y, y_pred)
             report_b05 = precision_recall_fscore_support(y, y_pred, beta=0.5)
             report_b2 = precision_recall_fscore_support(y, y_pred, beta=2)
-            results.append({                                                                                                
+            results.append({   
+                "model_name": Info.Name,
                 "threshold": t,
                 "precision_0": report[0][0],
                 "precision_0_05": report_b05[0][0],
@@ -38,7 +39,7 @@ class Analysis:
         df_results = pd.DataFrame(results)        
         exists = not os.path.exists('D:output/results/all_thresolds.csv')        
         
-        df_results.to_csv(f'{Info.PATH}/{Info.Name}_{Info.Optim}_automatic_results.csv', index=False, sep=';', header=True, decimal=",")
+        df_results.to_csv(f'{Info.PATH}/automatic_results.csv', index=False, sep=';', header=True, decimal=",")
         
         df_results.to_csv('D:output/results/all_thresolds.csv', mode='a', header=exists, index=False, sep=';', decimal=",")        
 
@@ -47,11 +48,11 @@ class Analysis:
         display = metrics.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc,
                                           estimator_name='estimator')
         display.plot()                
-        display.figure_.savefig(f"{Info.PATH}/{Info.Name}_{Info.Optim}_estimated_positives.png")
+        display.figure_.savefig(f"{Info.PATH}/estimated_positives.png")
 
     @staticmethod
     def best_thresolds():
-        df = pd.read_csv('D:output/results/all_thresolds.csv')
+        df = pd.read_csv('D:output/results/all_thresolds.csv')        
         idx = df.groupby('model_name')['acc'].idxmax()
         best_df = df.iloc[idx]
         best_df.to_csv('D:output/results/best_thresolds.csv', index=False, header=True, sep=';', decimal=",")
