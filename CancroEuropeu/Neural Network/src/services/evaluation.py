7e0e359f070aa81d3c6b14170173f6e49bad3c49
nysalvi <nysalvi20@gmail.com>
2023-05-22 23:41:16 -0300

    Added F-Score beta=0.5; Code has been readapted for new structure of single class output. From torch.max(output, 1) to (output >= 0.5).float()

--- CancroEuropeu/Neural Network/src/services/evaluation.py
+++ CancroEuropeu/Neural Network/src/services/evaluation.py
@@ -4,7 +4,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, fbeta_score
 from ..utils.global_info import Info
 from array import array
 import pandas as pd
@@ -27,63 +27,52 @@ class Evaluation:
             'prec': precision_score(y_true, y_pred, pos_label=pos_label),
             'recall': recall_score(y_true, y_pred, pos_label=pos_label),
             'f1': f1_score(y_true, y_pred, pos_label=pos_label),
+            'fbeta': fbeta_score(y_true, y_pred, beta=0.5, pos_label=pos_label),
             'loss': mean_absolute_error(y_true, y_pred)
         }
 
     def predict(self, model, loader) -> any:
         y_true = []
         y_pred = []
-        outputs_0 = []
-        outputs_1 = []
-        y_true_output = []
-        class_0 = []
-        class_1 = []
+        outputs = []
+        scores = []        
 
         for X, y in loader:
             X, y = X.to(Info.Device), y.to(Info.Device)
+            score = model(X).squeeze()                                    
+            scores+= score.tolist()
 
-            output = model(X)
+            outputs = (score>=0.5).float()
+            y_pred += outputs.tolist()
+            y_true += y.tolist()        
+        stack = np.stack((y_pred, y_true, scores), axis=1)
+        return y_true, y_pred, stack
 
-            _, y_pred_ = torch.max(output, 1)
-            for y_ in y.cpu():
-                y_true.append(y_)
-                y_true_output.append(y_.item())
-            for y_ in y_pred_.cpu():
-                y_pred.append(y_)
-            for y_ in output:
-                outputs_0.append(y_.detach().cpu().numpy()[0])
-                outputs_1.append(y_.detach().cpu().numpy()[1])
-                softmax_0, softmax_1 = self.calculate_softmax(y_.detach().cpu().numpy()[0], y_.detach().cpu().numpy()[1])
-                class_0.append(softmax_0)
-                class_1.append(softmax_1)
-
-        output_stacked = np.stack((outputs_0, outputs_1, y_true_output, class_0, class_1), axis=1)
-
-        return y_true, y_pred, output_stacked
-
-    def calculate_result(self, model_ft, test_loader):
-        y_true, y_pred, output_stacked = self.predict(model_ft, test_loader)
-        h_list_df = pd.DataFrame(output_stacked)                
-        h_list_df.to_csv(f'{Info.PATH}/{Info.Name}_{Info.Optim}.csv', 
-            index=False, sep=';', header=["outputs_0", "outputs_1", "true_values", "class_0", "class_1"], decimal=",")
+    def calculate_result(self, model_ft, test_loader, epochs):
+        y_true, y_pred, stack = self.predict(model_ft, test_loader)
+        h_list_df = pd.DataFrame(stack)       
+        print(h_list_df)
+        h_list_df.to_csv(f'{Info.PATH}/result.csv', 
+            index=False, sep=';', header=["outputs", "true_values", "scores"], decimal=",")
 
         h_val = self.evaluate_model(y_true, y_pred)
         h_val['model_name'] = Info.Name
         h_val['LR'] = Info.LR
         h_val['momen'] = Info.Momentum
-        h_val['epochs'] = Info.Epoch
-        #h_val['metric'] = Info.SaveType 
-        #h_val['LR'] = Info.WeightDecay
-        #h_val['lr_decay'] = Info.LR_Decay                
+        h_val['Epochs'] = epochs
+        h_val['metric'] = Info.SaveType 
+        h_val['Weight_Decay'] = Info.WeightDecay         
+        h_val['lr_decay'] = Info.LR_Decay                
+
         self.h_list_val.append(h_val)        
         self.append_results()
 
-        Info.Writer.add_scalar(f"{Info.BoardX}/Test/Loss", h_val['loss'])
-        Info.Writer.add_scalar(f"{Info.BoardX}/Test/Accuracy", h_val['acc'])
-        Info.Writer.add_scalar(f"{Info.BoardX}/Test/F1", h_val['f1'])
-        Info.Writer.add_scalar(f"{Info.BoardX}/Test/Precision", h_val['prec'])
-        Info.Writer.add_scalar(f"{Info.BoardX}/Test/Recall", h_val['recall'])
-        Info.Writer.flush()
+        #Info.Writer.add_scalar(f"{Info.Name}/Test/Loss", h_val['loss'])
+        #Info.Writer.add_scalar(f"{Info.Name}/Test/Accuracy", h_val['acc'])
+        #Info.Writer.add_scalar(f"{Info.Name}/Test/F1", h_val['f1'])
+        #Info.Writer.add_scalar(f"{Info.Name}/Test/Precision", h_val['prec'])
+        #Info.Writer.add_scalar(f"{Info.Name}/Test/Recall", h_val['recall'])
+        #Info.Writer.flush()
 
     def append_results(self):
         h_list_df = pd.DataFrame(self.h_list_val)      
@@ -91,15 +80,10 @@ class Evaluation:
         exists = not os.path.exists('D:output/results/all_results.csv')        
         h_list_df.to_csv('D:output/results/all_results.csv', mode='a', header=exists, index=False, sep=';', decimal=",")        
 
-    def calculate_softmax(self, output1, output2) -> tuple[float, float]:
-        class_0 = math.exp(output1) / (math.exp(output1) + math.exp(output2))
-        class_1 = math.exp(output2) / (math.exp(output1) + math.exp(output2))
-        return class_0, class_1
-
     @staticmethod
     def best_results():                
-        df = pd.read_csv('D:output/results/all_results.csv', mode='r', sep=';', decimal=',')
-        idx = df.groupby('model_name')['acc'].idxmax()
+        df = pd.read_csv('D:output/results/all_results.csv', sep=';', decimal=',')
+        idx = df.groupby('model_name')['fbeta'].idxmax()
         best_df = df.iloc[idx]
         best_df.to_csv('D:output/results/best_results.csv', index=False, header=True, sep=';', decimal=",")
         
