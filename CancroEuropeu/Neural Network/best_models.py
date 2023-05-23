01fba1d44143dfe054f5da7e69a8ccd9b27577d3
nysalvi <nysalvi20@gmail.com>
2023-05-22 23:19:24 -0300

    Fixed incorrect import structure

---
+++ CancroEuropeu/Neural Network/best_models.py
@@ -0,0 +1,6 @@
+from src.services.analysis import Analysis
+from src.services.evaluation import Evaluation
+
+if __name__ == "__main__":
+    Analysis.best_thresolds()
+    Evaluation.best_results()
