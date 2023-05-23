29232edd18da0abea3a2193d1291448fd8853776
nysalvi <nysalvi20@gmail.com>
2023-05-22 23:35:13 -0300

    Changed CrossEntropyLoss to BinaryCrossEntropyWithLogitsLoss (nn.BCEWithLogitsLoss). Sigmoid is applied internaly in BCE; Model output now returns a single probability. No probability for class 0 and class 1

--- CancroEuropeu/Neural Network/main.py
+++ CancroEuropeu/Neural Network/main.py
@@ -6,12 +6,14 @@ from src.services.evaluation import Evaluation
 from src.services.training import Training
 from src.services.analysis import Analysis
 from src.utils.global_info import Info
-from .src.utils.folder import Folder
+#from src.utils.folder import Folder
 #from src.utils.images import Images
 from src.utils.models import Models
+from torchvision import models
 from numpy import random
 from torch import nn
 import warnings
+import torch
 import os
 
 warnings.filterwarnings('ignore')
@@ -22,44 +24,45 @@ constants = Info.args()
 batch_size = constants['batch_size']
 num_classes = constants['num_classes']
 image_size = constants['image_size']
-feature_extract = constants['no-feature_extract']
-use_pretrained = constants['no-pre_trained']
+feature_extract = constants['no_feature_extract']
+use_pretrained = constants['no_pre_trained']
 num_epochs = constants['num_epochs']
 
+
 #End constants
-
-models = Models.select_models(Info.Name)
-
+selected_models = Models.select_models(ModelName[Info.Name])
 #Images.download(currentTempFolder, "data.zip")
 #Images.unzip(currentTempFolder, "data.zip")
 
-preProcessing = PreProcessing(Info.DataPath, (image_size, image_size))
-neuralData = preProcessing.run()
-
-preLoading = PreLoading()
-label_desc = preLoading.classDistribution(neuralData.train_data)
-preLoading.dataInstances(neuralData.train_data, neuralData.dev_data, neuralData.test_data)
-neuralLoader = preLoading.dataLoaders(batch_size, neuralData.train_data, neuralData.dev_data, neuralData.test_data)
-device = preLoading.selectDevice()
 
 evaluation = Evaluation()
-os.makedirs(f'{Info.PATH}', exist_ok=True)
-for neural_model_name in models:    
+
+for neural_model_name in selected_models:    
     Info.update_path(neural_model_name.name)
+    os.makedirs(Info.PATH, exist_ok=True)
+    os.makedirs(Info.BoardX, exist_ok=True)
+
+    preProcessing = PreProcessing(Info.DataPath, image_size, image_size)
+
+    neuralData = preProcessing.run()
+
+    preLoading = PreLoading()
+    label_desc = preLoading.classDistribution(neuralData.train_data)
+    device = preLoading.selectDevice()
+    #preLoading.dataInstances(neuralData.train_data, neuralData.dev_data, neuralData.test_data)
+    neuralLoader = preLoading.dataLoaders(batch_size, neuralData.train_data, neuralData.dev_data, neuralData.test_data)
+    #####################################################
 
     print(f"\n-------------------- Current model: {neural_model_name.name} --------------------\n")
     neuralNetworkModels = NeuralNetworkModels()
-    model_ft, input_size = neuralNetworkModels.initialize_model(neural_model_name, num_classes, feature_extract, use_pretrained = use_pretrained)
+    model_ft, input_size, grad_layer = neuralNetworkModels.initialize_model(neural_model_name, num_classes, feature_extract, use_pretrained = use_pretrained)
     modelFitData = neuralNetworkModels.optimize_model(feature_extract, model_ft, lr=Info.LR, momentum=Info.Momentum)
-    neuralNetworkModels.verify_predict_before_training(device, neuralLoader, modelFitData)
+    #neuralNetworkModels.verify_predict_before_training(device, neuralLoader, modelFitData)    
+    training = Training(torch.optim.lr_scheduler.CosineAnnealingLR,  modelFitData.optimizer_ft, num_epochs)
 
-    training = Training()
-    criterion = nn.CrossEntropyLoss().cuda() if device.isCuda else nn.CrossEntropyLoss()
-    dataframe, model_ft = training.train_and_evaluate(model_ft, num_epochs, neuralLoader.train_loader.loader, neuralLoader.dev_loader.loader, modelFitData.optimizer_ft, criterion)      
-
-    #torch.save(model_ft, f"{Info.PATH}/{Info.Name}_{Info.Optim}_model.pth")
-
-    training.verify_images(neuralLoader.test_loader.loader, batch_size, model_ft, label_desc)
+    criterion = nn.BCEWithLogitsLoss().cuda() if device.isCuda else nn.BCEWithLogitsLoss()
+    dataframe, model_ft = training.train_and_evaluate(model_ft, neuralLoader.train_loader.loader, neuralLoader.dev_loader.loader, criterion)      
     
-    evaluation.calculate_result(model_ft, neuralLoader.test_loader.loader)    
+    evaluation.calculate_result(model_ft, neuralLoader.test_loader.loader, num_epochs)    
+    training.verify_images(neuralLoader.test_loader.loader, batch_size, model_ft, label_desc, grad_layer)    
     Analysis.run()   
