d8d2ca448c970d1feaf4a78bd5c97a81a32de4db
nysalvi <nysalvi20@gmail.com>
2023-05-22 23:49:23 -0300

    Added CosineAnnealing learning rate decay

--- CancroEuropeu/Neural Network/src/services/training.py
+++ CancroEuropeu/Neural Network/src/services/training.py
@@ -10,92 +10,94 @@ import os
 from torch import nn, optim
 
 class Training:   
-    def train_epoch(self, model, trainLoader, optimizer, criterion):
+    def __init__(self, scheduler, optimizer, num_epochs):
+        self.optimizer = optimizer
+        self.num_epochs = num_epochs
+        self.scheduler = scheduler(optimizer, num_epochs)
+
+    def train_epoch(self, model, trainLoader, criterion):
         model.train()
         losses = []        
         for X, y in trainLoader:    
-            optimizer.zero_grad()
+            self.optimizer.zero_grad()
             X, y = X.to(Info.Device), y.to(Info.Device)            
             # (1) Passar os dados pela rede neural (forward)
-            output = model(X)          
+            output = model(X).squeeze()
             # (2) Calcular o erro da saída da rede com a classe das instâncias (loss)                    
-            loss = criterion(output, y)        
+            loss = criterion(output, y.float())        
             # (3) Usar o erro para calcular quanto cada peso (wi) contribuiu com esse erro (backward)
             loss.backward()
             # (4) Ataulizar os pesos da rede neural
-            optimizer.step()        
+            self.optimizer.step()        
             losses.append(loss.item())        
-
-        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 50)
-        scheduler.step()
+        
+        self.scheduler.step()
         model.eval()
         return np.mean(losses)
 
-    def eval_model(self, model, loader, criterion):
-        measures = []
+    def eval_model(self, model, loader, criterion):        
         total = 0
         correct = 0
         losses = []
         ground_truth = []
         prediction = []
+
         with torch.no_grad():
             for X, y in loader:                
                 X, y = X.to(Info.Device), y.to(Info.Device)      
                 ground_truth.append(y)
-                output = model(X)                      
-                _, y_pred = torch.max(output, 1)
+                output = model(X).squeeze()                      
+                y_pred = (output >= 0.5).float()
                 prediction.append(y_pred)
                 total += len(y)
-                loss = criterion(output, y)
+                loss = criterion(output, y.float())
                 losses.append(loss.item())             
                 correct += (y_pred == y).sum().cpu().data.numpy()                
 
         if Info.SaveType == 'Accuracy':
             metric = correct/total
         elif Info.SaveType == 'FScore':
-            metric = fbeta_score(ground_truth, prediction, 0.5)
-
+            metric = fbeta_score(ground_truth, prediction, 0.5)        
         measures = {'loss' : np.mean(losses), 'acc' : metric}
         return measures
 
-    def train_and_evaluate(self, model, num_epochs, train_loader, dev_loader, optimizer, criterion):                
+    def train_and_evaluate(self, model, train_loader, dev_loader, criterion):                
         max_metric = 0
-        contAcc = 0
+        contMetric = 0
         e_measures = []
-        pbar = tqdm(range(1,num_epochs+1))
+        pbar = tqdm(range(1,self.num_epochs+1))
         for e in pbar:
-            train_loss = self.train_epoch(model, train_loader, optimizer, criterion)
+            train_loss = self.train_epoch(model, train_loader, criterion)
             measures_on_train = self.eval_model(model, train_loader, criterion)
             measures_on_dev = self.eval_model(model, dev_loader, criterion)
             measures = {'epoch': e, 'train_loss': train_loss, 'train_acc' : measures_on_train['acc'].round(4), 
                 'dev_loss' : measures_on_dev['loss'], 'dev_acc' : measures_on_dev['acc'].round(4) }
             if (max_metric < measures_on_dev['acc'].round(4)):
-                contAcc = -1
+                contMetric = -1
                 max_metric = measures_on_dev['acc'].round(4)
                 torch.save(model.state_dict(), f'{Info.PATH}/state_dict.pt')
             
-            Info.Writer.add_scalar(f"{Info.BoardX}/Train/Loss", train_loss, e)
-            Info.Writer.add_scalar(f"{Info.BoardX}/Train/{Info.SaveType}", measures['train_acc'], e)
-            Info.Writer.add_scalar(f"{Info.BoardX}/Validation/Loss", measures['dev_loss'], e)
-            Info.Writer.add_scalar(f"{Info.BoardX}/Validation/{Info.SaveType}", measures['dev_acc'], e)
+            Info.Writer.add_scalar(f"{Info.Name}/Train/Loss", train_loss, e)
+            Info.Writer.add_scalar(f"{Info.Name}/Train/{Info.SaveType}", measures['train_acc'], e)
+            Info.Writer.add_scalar(f"{Info.Name}/Validation/Loss", measures['dev_loss'], e)
+            Info.Writer.add_scalar(f"{Info.Name}/Validation/{Info.SaveType}", measures['dev_acc'], e)
             Info.Writer.flush()
 
-            contAcc+= 1
-            if contAcc == 20:
+            contMetric+= 1
+            if contMetric == 20:
                 break
             pbar.set_postfix(measures)     
             e_measures += [measures]
         return pd.DataFrame(e_measures), model
 
-    def verify_images(self, test_loader, batch_size, model_ft, label_desc):
+    def verify_images(self, test_loader, batch_size, model_ft, label_desc, grad_layer):
         from pytorch_grad_cam import GradCAM
         from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
         from pytorch_grad_cam.utils.image import show_cam_on_image
         from torchvision import transforms
-
-        cam = GradCAM(model_ft, target_layers=target_layers, use_cuda=True)            
-        targets = [ClassifierOutputTarget(1)]
-        target_layers = [model_ft.layer4[-1]]
+        
+        cam = GradCAM(model_ft, target_layers=[grad_layer], use_cuda=True)            
+        targets = [ClassifierOutputTarget(0)]
 
         rows = 2
         columns = math.ceil(batch_size / rows)
@@ -106,14 +108,14 @@ class Training:
         for i, (X, y) in enumerate(test_loader):                
             X, y = X.to(Info.Device).requires_grad_(), y.numpy()
             fig = plt.figure(figsize=(24, 7))
-            for j in range(0, columns*rows):    
+            for j in range(0, list(X.shape)[0]):    
                 output = model_ft(X[j].unsqueeze(0))
                 _, y_pred = torch.max(output, 1)
                 y_pred = y_pred.cpu().data.numpy()
                 grayscale_cam = cam(input_tensor=X[j].unsqueeze(0), targets=targets)
                 grayscale_cam = grayscale_cam[0, :]
                 un_normed = un_minus(un_divide(X[j]))
-                un_normed = torch.transpose(X[j], 0, 2).cpu().detach().numpy()
+                un_normed = torch.transpose(un_normed, 0, 2).cpu().detach().numpy()
                 visualization = show_cam_on_image(un_normed, grayscale_cam, use_rgb=True)            
 
                 fig.add_subplot(rows, columns, j+1, title = 'Y(%i - %s) - Pred(%i - %s)' % (y[j], label_desc[y[j]], 
