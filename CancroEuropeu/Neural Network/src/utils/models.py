47e8cfb43217a34da71c11d35c0f14f7a976d489
nysalvi <nysalvi20@gmail.com>
2023-05-22 23:21:42 -0300

    Fixed not working method overload; Model selection properly works with enums AND STRINGS now;

--- CancroEuropeu/Neural Network/src/utils/models.py
+++ CancroEuropeu/Neural Network/src/utils/models.py
@@ -3,22 +3,22 @@ from ..model.enums.model_name_enum import ModelName
 
 class Models:
     @staticmethod
-    def select_models(model_name: ModelName) -> array:
+    def select_models(model_name: ModelName|str) -> array:
         models = []
-        if model_name == ModelName.ALL:
-            for model in ModelName:
-                if model != ModelName.ALL:
-                    models.append(model)
-        else:
-            models.append(model_name)
-        return models
-    @staticmethod
-    def select_models(model_name: str) -> array:       
-        models = []
-        if model_name == ModelName.ALL.name:
-            for model in ModelName:
-                if model != ModelName.ALL.name:
-                    models.append(ModelName[model_name])
-        else:
-            models.append(ModelName[model_name])
-        return models
+        if type(model_name) == ModelName:
+            if model_name == ModelName.ALL:
+                for model in ModelName:
+                    if model != ModelName.ALL:
+                        models.append(model)
+            else:
+                models.append(model_name)
+            return models
+        elif type(model_name) == str:
+            if model_name == ModelName.ALL.name:
+                for model in ModelName:
+                    if model != ModelName.ALL.name:
+                        models.append(ModelName[model_name])
+            else:
+                models.append(ModelName[model_name])
+            return models
+
