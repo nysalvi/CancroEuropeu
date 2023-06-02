ba500030e66f5a4c22c9c25955ca9841756d6fe1
nysalvi <nysalvi20@gmail.com>
2023-06-02 18:40:11 -0300

    Feature: Data transformations and  can now be set as an argument in config file

--- CancroEuropeu/Neural Network/src/services/pre_processing.py
+++ CancroEuropeu/Neural Network/src/services/pre_processing.py
@@ -8,7 +8,7 @@ def find_classes(self, directory: str) -> tuple[list[str], dict[str, int]]:
     idx_label = {}
     for root, dirs, files in os.walk(directory):
         if dirs == []:            
-            str_split = root.split('\\')[-1]
+            str_split = root.split(f'{os.sep}')[-1]
             pair = str_split.split(' - ') if len(str_split.split(' - ')) == 2 else False
             if pair:
                 num, label = pair     
@@ -18,37 +18,44 @@ def find_classes(self, directory: str) -> tuple[list[str], dict[str, int]]:
     return (labels, idx_label)
 
 class PreProcessing:
-    input_size: any
-    dataPath: str
-
     def __init__(self, dataPath, height, width) -> None:
         self.input_size = (height,width)
         self.dataPath = dataPath
         pass
 
-    def run(self) -> NeuralData:       
-        data_transforms = {
-        'train' : transforms.Compose([                
-                transforms.RandomResizedCrop(self.input_size),
-                #transforms.Resize(self.input_size),
-                transforms.RandomHorizontalFlip(),
-                transforms.RandomVerticalFlip(),
-                #transforms.ColorJitter(brightness=0.05, contrast=0.05, hue=0.05),                                            
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])                                                    
-                ]),
-        'dev' : transforms.Compose([                                    
-            transforms.Resize(self.input_size),        
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])            
-        ]),
-        'test'   : transforms.Compose([
-                transforms.Resize(self.input_size),        
+    def run(self, train_transform=None, dev_transform=None, test_transform=None) -> NeuralData:       
+
+        if train_transform:
+            train = eval(train_transform)
+        else:
+            train = transforms.Compose([                
+                transforms.Resize(self.input_size),
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
-        }
 
+        if dev_transform:
+            dev = eval(dev_transform)
+        else:
+            dev = transforms.Compose([                                    
+                transforms.Resize(self.input_size),        
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])            
+            ])
+
+        if test_transform:
+            test = eval(test_transform)
+        else:
+            test = transforms.Compose([                                    
+                transforms.Resize(self.input_size),        
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])            
+            ])
+        data_transforms = {
+            'train' : train,
+            'dev' : dev,
+            'test'   : test
+        }
         datasets.ImageFolder.find_classes = find_classes
         datasets.ImageFolder.make_dataset = make_dataset
 
