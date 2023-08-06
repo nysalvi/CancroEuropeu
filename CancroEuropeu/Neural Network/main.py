4a7a49c8f4629b663eae96b83ac2af101a85b767
nysalvi <nysalvi20@gmail.com>
2023-08-06 14:27:37 -0300

    Training will be resumed automatically, given specified model/path combination, instead of starting a new training

--- CancroEuropeu/Neural Network/main.py
+++ CancroEuropeu/Neural Network/main.py
@@ -13,9 +13,11 @@ from torchvision import models
 from numpy import random
 from torch import nn
 import warnings
+import pickle
 import torch
 import gc
 import os
+import io
 
 warnings.filterwarnings('ignore')
 random.RandomState(1)
@@ -25,9 +27,11 @@ constants = Info.args()
 batch_size = constants['batch_size']
 num_classes = constants['num_classes']
 feature_extract = constants['no_feature_extract']
-use_pretrained = constants['no_pre_trained']
 num_epochs = constants['num_epochs']
+resume_training = os.path.exists(f'{Info.PATH}{os.sep}state_dict.pt') and not os.path.exists(
+    f'{Info.PATH}{os.sep}finished_dict.pt')
 
+        
 #End constants
 selected_models = Models.select_models(ModelName[Info.Name])
 #Images.download(currentTempFolder, "data.zip")
@@ -38,10 +42,16 @@ evaluation = Evaluation()
     
 for neural_model_name in selected_models:    
     Info.update_path(neural_model_name.name)
+    if resume_training:    
+        f = io.FileIO(f'{Info.PATH}{os.sep}stats.txt', 'r')
+        Info = pickle.load(f)
+    os.makedirs(Info.PATH, exist_ok=True)
+    os.makedirs(Info.BoardX, exist_ok=True)
 
     print(f"\n-------------------- Current model: {neural_model_name.name} --------------------\n")
     neuralNetworkModels = NeuralNetworkModels()
-    model_ft, input_size, grad_layer = neuralNetworkModels.initialize_model(neural_model_name, num_classes, feature_extract, use_pretrained = use_pretrained)
+    model_ft, input_size, grad_layer = neuralNetworkModels.initialize_model(neural_model_name, num_classes, 
+        feature_extract, use_pretrained = resume_training)
 
     preProcessing = PreProcessing(Info.DataPath, input_size, input_size)
     neuralData = preProcessing.run()
@@ -53,9 +63,10 @@ for neural_model_name in selected_models:
     modelFitData = neuralNetworkModels.optimize_model(feature_extract, model_ft, lr=Info.LR, momentum=Info.Momentum)    
     training = Training(torch.optim.lr_scheduler.CosineAnnealingLR,  modelFitData.optimizer_ft, num_epochs)
     criterion = nn.BCEWithLogitsLoss().cuda() if device.isCuda else nn.BCEWithLogitsLoss()
-    dataframe, model_ft = training.train_and_evaluate(model_ft, neuralLoader.train_loader.loader, neuralLoader.dev_loader.loader, criterion)      
+    dataframe, model_ft = training.train_and_evaluate(model_ft, neuralLoader.train_loader.loader, 
+        neuralLoader.dev_loader.loader, criterion)      
     evaluation.calculate_result(model_ft, neuralLoader.test_loader.loader, num_epochs)    
-    #training.verify_images(neuralLoader.test_loader.loader, batch_size, model_ft, label_desc, grad_layer)    
+    #training.verify_images("last_conv", neuralLoader.test_loader.loader, batch_size, model_ft, label_desc, grad_layer)        
     Analysis.run()   
     
     #del criterion
